4707c98e38c53440585c9afe047d383825419ae1
Luis Garcia <luisfergar99@gmail.com>
2017-08-04 15:52:30 -0700

    javascript

---
+++ hello.js
@@ -0,0 +1,51 @@
+function fadeImage(){
+    $('img').fadeToggle(4000,'swing');
+}
+function addBorderToMap(){
+    $('#map').css({border:'10px solid black'})
+}
+
+function fadeOutCurrentElement(){
+    $(this).fadeOut(4000,'swing')
+}
+
+function houseMusic()
+{
+    alert("House Music: Boots & Cats & Boots & Cats")
+}
+
+
+function bounceTitle(){       $(".title").animate({left:'100%'},10000);
+$(".title").animate({left:'-10%'},10000); 
+}
+function textGrow(){
+    $(this).animate({fontSize: 40},6000).animate({fontSize:24},2000);
+}
+
+function marquee(target){    $(target).animate({left:'100%'},10000)
+}
+function showAudioBar(){
+    alert("Get ready to hear my favorite tune!!!")
+    $('audio').css({display:"block"})
+}
+
+function sayHello(){
+  var userText = $('#message').val();
+    $('#result').text("Hello, "+userText +". Nice to meet you!");
+}
+
+function setUpHandlers(){
+    $('.suzu').on('click',fadeImage);
+    $('.fader').on('click', fadeImage);
+    $('#map').on('mouseenter',addBorderToMap);
+    $('li').on('click',fadeOutCurrentElement);
+   // $('button').on('click', houseMusic);
+    $('button').on('click',showAudioBar);
+    $('#message').on('input', sayHello)
+    $('body').on('mouseenter',bounceTitle);
+
+     //$('.title').on('mouseenter',textGrow);
+
+}
+
+$(document).ready(setUpHandlers);
